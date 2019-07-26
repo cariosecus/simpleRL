3642f70b6f7992e260938b1034c937a4931dddd6
cariosecus <cariosecus@gmail.com>
2019-07-27 00:46:17 +0100

    messages

--- death_functions.py
+++ death_functions.py
@@ -1,17 +1,17 @@
 import tcod as libtcod
 from render_functions import RenderOrder
 from game_states import GameStates
-
+from game_messages import Message
 
 def kill_player(player):
     player.char = '%'
     player.color = libtcod.dark_red
 
-    return 'You died!', GameStates.PLAYER_DEAD
+    return Message('You died!', libtcod.red), GameStates.PLAYER_DEAD
 
 
 def kill_npc(npc):
-    death_message = '{0} is dead!'.format(npc.name.capitalize())
+    death_message = Message('{0} is dead!'.format(npc.name.capitalize()), libtcod.orange)
 
     npc.char = '%'
     npc.color = libtcod.dark_red
