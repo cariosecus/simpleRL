98e427c6be5cf1876075e92d7ef699aa41ffb5bc
cariosecus <cariosecus@gmail.com>
2019-08-03 03:21:59 +0100

    increasing difficulty

--- loader_functions/initialize_new_game.py
+++ loader_functions/initialize_new_game.py
@@ -67,7 +67,7 @@ def get_constants():
     return constants
 
 def get_game_variables(constants):
-    fighter_component = Fighter(hp=30, defense=2, power=5)
+    fighter_component = Fighter(hp=100, defense=1, power=4)
     inventory_component = Inventory(26)
     level_component = Level()
     player = Entity(0, 0, '@', libtcod.white, 'Player', blocks=True, render_order=RenderOrder.ACTOR,
@@ -75,7 +75,7 @@ def get_game_variables(constants):
     entities = [player]
 
     game_map = GameMap(constants['screen_width'], constants['screen_height'])
-    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['screen_width'], constants['screen_height'], player, entities, constants['max_monsters_per_room'], constants['max_items_per_room'])
+    game_map.make_map(constants['max_rooms'], constants['room_min_size'], constants['room_max_size'], constants['screen_width'], constants['screen_height'], player, entities)
     message_log = MessageLog(constants['message_x'], constants['message_width'], constants['message_height'])
 
     game_state = GameStates.PLAYERS_TURN
