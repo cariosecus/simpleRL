83213ceac70f3bd70393c01afe04cf06ad2afdd5
cariosecus <cariosecus@gmail.com>
2019-08-08 00:45:56 +0100

    refactoring inputs

--- input_handlers.py
+++ input_handlers.py
@@ -25,71 +25,22 @@ class InputHandler(libtcod.event.EventDispatch):
 			self._actionq.append({"exit": True})
 
 		if self.state == GameStates.PLAYERS_TURN:
-			# movement keys
-			if event.sym == libtcod.event.K_UP:
-				self._actionq.append({"move": (0, -1)})
-			elif event.sym == libtcod.event.K_DOWN:
-				self._actionq.append({"move": (0, 1)})
-			elif event.sym == libtcod.event.K_LEFT:
-				self._actionq.append({"move": (-1, 0)})
-			elif event.sym == libtcod.event.K_RIGHT:
-				self._actionq.append({"move": (1, 0)})
-			elif event.sym == libtcod.event.K_w:
-				self._actionq.append({"move": (0, -1)})
-			elif event.sym == libtcod.event.K_s:
-				self._actionq.append({"move": (0, 1)})
-			elif event.sym == libtcod.event.K_a:
-				self._actionq.append({"move": (-1, 0)})
-			elif event.sym == libtcod.event.K_d:
-				self._actionq.append({"move": (1, 0)})
-			elif event.sym == libtcod.event.K_q:
-				self._actionq.append({"move": (-1, -1)})
-			elif event.sym == libtcod.event.K_e:
-				self._actionq.append({"move": (1, -1)})
-			elif event.sym == libtcod.event.K_z:
-				self._actionq.append({"move": (-1, 1)})
-			elif event.sym == libtcod.event.K_c:
-				self._actionq.append({"move": (1, 1)})
-				# pick up
-			elif event.sym == libtcod.event.K_g:
-				self._actionq.append({"pickup": True})
-				# inventory
-			elif event.sym == libtcod.event.K_i:
-				self._actionq.append({"show_inventory": True})
-			elif event.sym == libtcod.event.K_u:
-				self._actionq.append({"drop_inventory": True})
-			elif event.sym == libtcod.event.K_t:
-				self._actionq.append({"show_character_screen": True})
-				# use
-			elif event.sym == libtcod.event.K_f:
-				self._actionq.append({"take_stairs": True})
-			elif event.sym == libtcod.event.K_x:
-				self._actionq.append({"wait": True})
+			if event.sym in keymap_players_turn.keys():
+				self._actionq.append(keymap_players_turn[event.sym])
 		elif self.state == GameStates.PLAYER_DEAD:
-			if event.sym == libtcod.event.K_i:
-				self._actionq.append({"show_inventory": True})
+			if event.sym in keymap_player_dead.keys():
+				self._actionq.append(keymap_player_dead[event.sym])
+		elif self.state == GameStates.LEVEL_UP:
+			if event.sym in keymap_level_up.keys():
+				self._actionq.append(keymap_level_up[event.sym])
+		elif self.state == GameStates.MAIN_MENU:
+			if event.sym in keymap_main_menu.keys():
+				self._actionq.append(keymap_main_menu[event.sym])
 
 		elif self.state in (GameStates.SHOW_INVENTORY, GameStates.DROP_INVENTORY, GameStates.TARGETING):
 			inv_index = event.sym - ord("a")
 			if 0 <= inv_index < 26:
 				self._actionq.append({"inventory_index": inv_index})
-
-		elif self.state == GameStates.LEVEL_UP:
-			if event.sym == libtcod.event.K_a:
-				self._actionq.append({'level_up': 'hp'})
-			elif event.sym == libtcod.event.K_b:
-				self._actionq.append({'level_up': 'str'})
-			elif event.sym == libtcod.event.K_c:
-				self._actionq.append({'level_up': 'def'})
-			return {}
-
-		elif self.state == GameStates.MAIN_MENU:
-			if event.sym == libtcod.event.K_a:
-				self._actionq.append({'new_game': True})
-			elif event.sym == libtcod.event.K_b:
-				self._actionq.append({'load_game': True})
-			elif event.sym == libtcod.event.K_c:
-				self._actionq.append({"exit": True})
 
 	def ev_mousemotion(self, event):
 		x, y = event.tile
@@ -111,3 +62,43 @@ class InputHandler(libtcod.event.EventDispatch):
 
 	def clear_actionq(self):
 		self._actionq.clear()
+
+
+keymap_players_turn = {
+		# movement keys
+		libtcod.event.K_UP: {"move": (0, -1)},
+		libtcod.event.K_DOWN: {"move": (0, 1)},
+		libtcod.event.K_LEFT: {"move": (-1, 0)},
+		libtcod.event.K_RIGHT: {"move": (1, 0)},
+		libtcod.event.K_w: {"move": (0, -1)},
+		libtcod.event.K_s: {"move": (0, 1)},
+		libtcod.event.K_a: {"move": (-1, 0)},
+		libtcod.event.K_d: {"move": (1, 0)},
+		libtcod.event.K_q: {"move": (-1, -1)},
+		libtcod.event.K_e: {"move": (1, -1)},
+		libtcod.event.K_z: {"move": (-1, 1)},
+		libtcod.event.K_c: {"move": (1, 1)},
+		# pick up
+		libtcod.event.K_g: {"pickup": True},
+		# inventory
+		libtcod.event.K_i: {"show_inventory": True},
+		libtcod.event.K_u: {"drop_inventory": True},
+		libtcod.event.K_t: {"show_character_screen": True},
+		# use
+		libtcod.event.K_f: {"take_stairs": True},
+		libtcod.event.K_x: {"wait": True},
+		}
+keymap_player_dead = {
+		libtcod.event.K_i: {"show_inventory": True},
+		}
+
+keymap_level_up = {
+		libtcod.event.K_a: {'level_up': 'hp'},
+		libtcod.event.K_b: {'level_up': 'str'},
+		libtcod.event.K_c: {'level_up': 'def'},
+		}
+keymap_main_menu = {
+		libtcod.event.K_a: {'new_game': True},
+		libtcod.event.K_b: {'load_game': True},
+		libtcod.event.K_c: {"exit": True},
+		}
